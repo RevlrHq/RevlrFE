0acb5216161f56b81414255446fc186ac6abd579
adelowomi <adelowo.ajibola@m-kopa.com>
2025-03-07 01:30:01 +0100

    refactor: remove unused authentication components and hooks

--- src/app/(auth)/forgot-password/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ForgotPassword from '@features/auth/components/ForgotPassword';
-
-export default function page() {
-    return <ForgotPassword />;
-}
--- src/app/(auth)/signup/page.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Register from '@features/auth/components/Register';
-
-export default function page() {
-    return <Register />;
-}
